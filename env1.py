9ae0d25c5fe8472d73e20160727ae1dfb0a59bc8
gnuespy <15sally51@naver.com>
2024-10-31 15:18:02 +0900

    env1.py

--- env1.py
+++ env1.py
@@ -5,8 +5,9 @@ from google.cloud import bigquery
 # 서비스 계정 JSON 파일의 경로를 환경 변수로 설정
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "semiotic-vial-440207-q4-898f401704c0.json"
 
-# BigQuery 클라이언트 생성
-client = bigquery.Client()
+# BigQuery 클라이언트 생성 (프로젝트 ID를 명시적으로 지정)
+project_id = "semiotic-vial-440207-q4"
+client = bigquery.Client(project=project_id)
 
 # Streamlit 앱 UI
 st.title("Streamlit to GCP BigQuery")
@@ -17,9 +18,8 @@ name = st.text_input("Name")
 age = st.number_input("Age", min_value=0, step=1)
 
 # BigQuery 테이블 설정
-project_id = "semiotic-vial-440207-q4"  # 사용자의 GCP 프로젝트 ID
-dataset_id = "json_test"                  # 사용자의 데이터셋 이름
-table_id = "userstest"                    # 테이블 이름
+dataset_id = "json_test"
+table_id = "userstest"
 table_ref = f"{project_id}.{dataset_id}.{table_id}"
 
 # 데이터 삽입 버튼
