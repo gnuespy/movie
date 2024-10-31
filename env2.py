28d419ac772ac2ef4e45f761e4145994e01a4155
gnuespy <15sally51@naver.com>
2024-10-31 15:56:58 +0900

    env2.py

--- env2.py
+++ env2.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from google.cloud import bigquery
 
 # 서비스 계정 JSON 파일의 경로를 환경 변수로 설정
@@ -8,11 +9,14 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "semiotic-vial-440207-q4-8235ac99
 project_id = "semiotic-vial-440207-q4"
 client = bigquery.Client(project=project_id)
 
+# Streamlit 앱 UI
+st.title("BigQuery Datasets Check")
+
 # BigQuery 데이터 세트 확인
 try:
     datasets = client.list_datasets()  # 데이터 세트 목록 가져오기
-    print("Datasets in project:")
+    st.write("Datasets in project:")
     for dataset in datasets:
-        print(dataset.dataset_id)
+        st.write(dataset.dataset_id)
 except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    st.error(f"An error occurred: {str(e)}")
