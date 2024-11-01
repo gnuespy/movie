66d7c835e81294d9c850d2fce7bf2f9fba5a65c4
gnuespy <15sally51@naver.com>
2024-11-01 15:39:34 +0900

    env1.py

--- env1.py
+++ env1.py
@@ -1,31 +1,13 @@
-import os
 import streamlit as st
-from google.cloud import secretmanager
 from google.cloud import bigquery
-import json
-import tempfile
+from gcp_auth import setup_google_cloud_credentials
 
 # Google Cloud 프로젝트 ID 및 비밀 이름
 project_id = "semiotic-vial-440207-q4"  # 올바른 프로젝트 ID 사용
-secret_id = "moviejj"
+secret_id = "moviejj"  # Secret Manager에 저장된 비밀 이름
 
-# Secret Manager 클라이언트 생성
-client = secretmanager.SecretManagerServiceClient()
-
-# 비밀의 최신 버전 가져오기
-secret_version_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
-response = client.access_secret_version(name=secret_version_name)
-
-# 비밀의 내용을 JSON으로 변환
-service_account_info = json.loads(response.payload.data.decode("UTF-8"))
-
-# 임시 파일에 서비스 계정 JSON 저장
-with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp_file:
-    temp_file.write(json.dumps(service_account_info).encode("UTF-8"))
-    service_account_file_path = temp_file.name
-
-# 서비스 계정 JSON 파일의 경로를 환경 변수로 설정
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_file_path
+# Google Cloud 인증 설정
+setup_google_cloud_credentials(project_id, secret_id)
 
 # BigQuery 클라이언트 생성
 bigquery_client = bigquery.Client(project=project_id)
@@ -52,7 +34,7 @@ if st.button("Insert into BigQuery"):
         
         errors = bigquery_client.insert_rows_json(table_ref, rows_to_insert)
         
-        if errors == []:
+        if not errors:
             st.success("Data inserted successfully!")
         else:
             st.error(f"Encountered errors while inserting: {errors}")
